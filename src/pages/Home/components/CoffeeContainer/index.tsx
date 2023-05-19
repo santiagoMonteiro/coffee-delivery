bf09cf342bf8e1734635381c8f26a2bc91905313
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-05-19 10:53:02 -0400

    coffee data mapping

--- src/pages/Home/components/CoffeeContainer/index.tsx
+++ src/pages/Home/components/CoffeeContainer/index.tsx
@@ -1,6 +1,6 @@
 import { coffeeSchema } from '../CoffeeCard/coffeeSchema'
 import { CoffeeCard } from '../CoffeeCard'
-// import styles from './styles.module.scss'
+import styles from './styles.module.scss'
 
 export function CoffeeContainer() {
   return (
